5c039eb366117ad5dc4052b2534643bc72439115
yunheur <yunheur@gmail.com>
2021-03-25 17:19:57 +0900

    spring-batch-lightmin-client-jdbc

---
+++ client-jdbc/src/main/kotlin/com/yunheur/client/ClientApplication.kt
@@ -0,0 +1,15 @@
+package com.yunheur.client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.tuxdevelop.spring.batch.lightmin.client.classic.annotation.EnableLightminClientClassic
+import org.tuxdevelop.spring.batch.lightmin.repository.annotation.EnableLightminJdbcConfigurationRepository
+
+@EnableLightminClientClassic
+@EnableLightminJdbcConfigurationRepository
+@SpringBootApplication
+class ClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientApplication>(*args)
+}
